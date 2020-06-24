e397c2fd8944254d0fb7c7a706efffb52c3734cc
mayon <frontender@126.com>
2020-06-24 20:56:23 +0800

    (week01): add .gitignore.

--- week01/1_requests.py
+++ week01/1_requests.py
@@ -22,21 +22,31 @@ def get_user_agent():
   return random.choice(ua_list)
 
 def get_page_text(url):
-  headers = {'user-agent': get_user_agent()}
+  headers = {
+    'user-agent': get_user_agent(),
+    'Accept': "*/*",
+    'Accept-Language': 'en-AU,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,la;q=0.6',
+    'Content-Type': 'text/plain',
+    'Connection': 'keep-alive',
+    'Origin': 'https://maoyan.com',
+    'Referer': 'https://maoyan.com/films?showType=3',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
+  }
   response = requests.get(url, headers=headers)
   print("------------------")
   print('Request Url      :', url)
   print('Response Status  :', response.status_code)
-  print('Page Title       :', re.findall(r'<title>(.+?)</title>', response.text)[0] | 'null')
-  print("------------------")
   return response.text;
 
-def robbot(listUrl, multiplier, find_film_urls_fun, xpaths, save_path):
+def robot(listUrl, multiplier, find_film_urls_fun, xpaths, save_path):
   # Get film urls
   film_urls = []
-  list_urls = tuple((re.sub('{page}', str(page * multiplier), listUrl)) for page in range(2))
+  list_urls = tuple((re.sub('{page}', str(page * multiplier), listUrl)) for page in range(1))
   for list_url in list_urls:
-    film_urls += find_film_urls_fun(bs(get_page_text(list_url), 'html.parser'))
+    bs_info = bs(get_page_text(list_url), 'html.parser')
+    film_urls += find_film_urls_fun(bs_info)
     sleep(2)
 
   # Get film infos
@@ -45,8 +55,8 @@ def robbot(listUrl, multiplier, find_film_urls_fun, xpaths, save_path):
     selector = lxml.etree.HTML(get_page_text(film_url))
     for lxml_xpath in xpaths:
       lxml_info = selector.xpath(lxml_xpath)
-      film_infos.append(lxml_info);
-      print(lxml_info)
+      if (len(lxml_info)):
+        film_infos.append(lxml_info[0]);
     sleep(2)
 
   # Sava film infos
@@ -61,23 +71,26 @@ def doubanRobot():
     '//*[@id="info"]/span[10]/text()',
     '//*[@id="interest_sectl"]/div[1]/div[2]/strong/text()'
   ];
-  db_save_path = '1_db_requests'
+  db_save_path = './Python001-class01/week01/1_db_requests'
 
   def db_find_film_urls(info):
     urls = []
     for tags in info.findAll('div', attrs={ 'class': 'hd' }):
       for atag in tags.findAll('a'):
         urls.append(atag.get('href'))
+    print('Douban Film Urls : ', urls)
     return urls
 
-  robbot(db_list_url, db_multiplier, db_find_film_urls, db_xpaths, db_save_path)
+  robot(db_list_url, db_multiplier, db_find_film_urls, db_xpaths, db_save_path)
 
 def maoyanRobot():
   def my_find_film_urls(info):
+    print('Maoyan Page Title: ', re.findall('<title>(.+?)</title>', str(info))[0])
     urls = []
     for tags in info.findAll('div', attrs={ 'class': 'movie-item-hover' }):
       for atag in tags.findAll('a'):
-        urls.append('https://maoyan.com/' + atag.get('href'))
+        urls.append('https://maoyan.com' + atag.get('href'))
+    print('Maoyan Film Urls : ', urls)
     return urls
 
   my_list_url = 'https://maoyan.com/films?showType=2&offset={page}'
@@ -87,13 +100,13 @@ def maoyanRobot():
     '//*[@class="movie-brief-container"]/h1',
     '//*[@class="movie-brief-container"]/div',
     '//*[@class="movie-brief-container"]/ul/li[1]/a[1]',
-    '//*[@class="conmovie-brief-containertent"]/ul/li[1]/a[2]',
+    '//*[@class="movie-brief-container"]/ul/li[1]/a[2]',
     '//*[@class="movie-brief-container"]/ul/li[3]'
   ];
 
-  my_save_path = '1_my_requests'
+  my_save_path = './Python001-class01/week01/1_my_requests'
 
-  robbot(my_list_url, my_multiplier, my_find_film_urls, my_xpaths, my_save_path)
+  robot(my_list_url, my_multiplier, my_find_film_urls, my_xpaths, my_save_path)
 
 # doubanRobot()
 maoyanRobot()
