2e55c0ebdfe42056e90c9d27c13f63c6aac79d77
mayon <frontender@126.com>
2020-09-06 01:05:09 +0800

    fix(week02): debug homework

--- week02/spiders/spiders/pipelines.py
+++ week02/spiders/spiders/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-# from mydb import ConnDB
+# from spiders.mydb import ConnDB
 
 class SpidersPipeline:
     # def process_item(self, item, spider):
